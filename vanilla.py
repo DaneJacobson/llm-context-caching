491cc324dc7f84c1ca9c10bc706d03ef6ae9fc53
Dane Jacobson <jacobson.dane@gmail.com>
2024-06-22 01:47:54 -0400

    Oldish commit

--- vanilla.py
+++ vanilla.py
@@ -45,7 +45,6 @@ class RMSNorm(torch.nn.Module):
         output = self._norm(x.float()).type_as(x)
         return output * self.weight
 
-
 def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
     freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
     t = torch.arange(end, device=freqs.device, dtype=torch.float32)
